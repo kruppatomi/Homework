6a63b5df1da8102e81ec8a9f3ba20b7e0efe1355
kruppatomi <hungaryca@gmail.com>
2020-08-13 20:39:15 +0200

    get forbidden strings in a list

--- Exercise2/src/main/java/App.java
+++ Exercise2/src/main/java/App.java
@@ -3,10 +3,8 @@ import java.io.File;
 public class App {
     public static void main(String[] args){
         ColumnsManager columnsManager = new ColumnsManager();
+        columnsManager.getForbiddenList();
 
-        final File folder = new File("C:/Users/Kruppa/Desktop/Testfolder");
-        Filereader filereader = new Filereader(folder);
-        filereader.getLines();
 
     }
 }
--- Exercise2/src/main/java/ColumnsManager.java
+++ Exercise2/src/main/java/ColumnsManager.java
@@ -1,3 +1,36 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ColumnsManager {
+
+    List<String> forbiddenStrings;
+
+    public ColumnsManager(){
+      forbiddenStrings = getForbiddenStrings();
+    }
+
+    //forbidden Substring dwXX
+    public void getTableWithoutSubstring(){
+        final File folder = new File("C:/Users/Kruppa/Desktop/Testfolder");
+        Filereader filereader = new Filereader(folder);
+        filereader.getLines();
+    }
+
+    public List<String> getForbiddenStrings(){
+        List<String> forbidden = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                forbidden.add("dw"+i+j);
+            }
+        }
+        return forbidden;
+    }
+
+
+    public List<String> getForbiddenList(){
+        return forbiddenStrings;
+    }
+
 
 }
